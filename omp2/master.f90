2b682cbc5b2dc3b0bdf716da0c4f4bdfb77aeb9a
tianli <tzeng@clarkson.edu>
2017-12-29 16:33:12 -0500

    add barrier

--- omp2/master.f90
+++ omp2/master.f90
@@ -1,7 +1,7 @@
 !master save to github
     implicit none
-    integer,parameter::num_threads=1
-    integer,parameter::n=12
+    integer,parameter::num_threads=8
+    integer,parameter::n=24
     integer::i,omp_get_thread_num
     real::a=100.0
     real,dimension(n)::b
@@ -18,6 +18,8 @@
       !$ a=real(thread_num)+200.0
     !$omp end master
     
+    !$omp barrier
+    
     !$ thread_num=omp_get_thread_num()
     !$ istart=thread_num*ppt+1
     !$ iend=min(n,thread_num*ppt+ppt)
@@ -30,7 +32,7 @@
     
     !$omp end parallel
     print*,"serial value of a=100.0"
-    !!$ print*,"parallel value of a=200"
+    !$ print*,"parallel value of a=200"
     do i=1,n
     print'("array b (",i2,")=",f15.6)',i,b(i)
       !  print'("aa,   (",i2,")   ",f15.6)',i,b(i)
