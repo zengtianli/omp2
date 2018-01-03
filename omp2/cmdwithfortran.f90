dc77ecc344565a954f2f16bfc596e90d34abb7f1
tianli <tzeng@clarkson.edu>
2018-01-02 23:38:03 -0500

    1version

---
+++ omp2/cmdwithfortran.f90
@@ -0,0 +1,59 @@
+! CALL GET_COMMAND(LENGTH=l1, STATUS=stat) LENGTH means this code in the direction name's length
+!PRINT "('The command line was:',A)"
+!    
+    
+    
+PROGRAM ByYourCommand
+  IMPLICIT NONE
+  CHARACTER(:), ALLOCATABLE :: full_command_line
+  CHARACTER(:), ALLOCATABLE :: a_single_argument
+  INTEGER :: l,c=10
+  INTEGER :: stat
+  !CHARACTER(5) :: cmd
+  !CALL get_command(cmd)
+  !WRITE (*,*) TRIM(cmd)  
+  
+  !CALL GET_COMMAND([COMMAND, LENGTH, STATUS])
+  
+  !****
+  CALL GET_COMMAND(LENGTH=l, STATUS=stat)!LENGTH means this code in the direction name's length
+  !, STATUS=stat
+ ! full_command_line='aaaaaaaaaa'
+  IF (stat > 0) THEN
+    STOP "Sorry, but I couldn't retrieve the command line"
+  END IF
+  
+  IF (l == 0) THEN
+    STOP "Sorry, but I couldn't work out the command length"
+  END IF
+  
+  ALLOCATE(CHARACTER(l) :: full_command_line)
+  ! ALLOCATE(CHARACTER(30) :: full_command_line)
+  CALL GET_COMMAND(COMMAND=full_command_line)!, STATUS=stat
+  
+  IF (stat /= 0) THEN
+    STOP 'Sorry, but GET_COMMAND failed unexpectedly'
+  END IF
+ ! PRINT "('The command line was:',I4.4)",C! full_command_line
+PRINT "('The command line was:',A)", full_command_line  
+  IF (COMMAND_ARGUMENT_COUNT() == 0) THEN
+    STOP "There were no command arguments"
+  END IF
+  
+  CALL GET_COMMAND_ARGUMENT(1, LENGTH=l, STATUS=stat)
+  IF (stat > 0) THEN
+    STOP "Sorry, but I couldn't retrieve the first command argument"
+  END IF
+  
+  IF (l == 0) THEN
+    STOP "Sorry, but I couldn't work out the length of the first argument"
+  END IF
+  
+  ALLOCATE(CHARACTER(l) :: a_single_argument)
+  CALL GET_COMMAND_ARGUMENT(1, VALUE=a_single_argument, STATUS=stat)
+  IF (stat /= 0) THEN
+    STOP 'Sorry, but GET_COMMAND_ARGUMENT failed unexpectedly'
+  END IF
+  PRINT "('The first argument was:',A)", a_single_argument
+  
+END PROGRAM ByYourCommand
