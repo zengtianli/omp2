463939a09b899d75eed8c56a6f32cbb55f37acaa
tianli <tzeng@clarkson.edu>
2018-01-02 21:18:27 -0500

    character format i5.2 fname=bbbbc status='new','old'

--- omp2/writeform.f90
+++ omp2/writeform.f90
@@ -1,19 +1,20 @@
 !==============================================
-! character::fname         !default of character size is 1
+! character::fname         !default of character size is 1// character(30)::fname size=30 
 !  format(i5.2)  result is 01, format(i5.5) result is 00005   
 !   fname='bcdddddd'  !only print b if character size is 1 
+!status='new'=non exist ,'old'=exist    
 !==============================================
     
 PROGRAM main
   IMPLICIT NONE
-  INTEGER, DIMENSION(:,:,:), ALLOCATABLE :: array
+  INTEGER, DIMENSION(:,:), ALLOCATABLE :: array
   integer::a,b,c,d,e
  ! character::fname         !default of character size is 1
   character(30)::fname     !10 means character size is 10
-  ALLOCATE (array(2,3,4))
+  ALLOCATE (array(2,3))
   a=size(array,1) 
   b=size(array,2)
-  c=size(array,3)
+ ! c=size(array,3)
   d=size(array)
   array=1.0
  ! array = transpose(reshape((/ 1, 2, 3, 4, 5, 6 /),(/ size(array, 2), size(array, 1)/))) 
@@ -27,10 +28,16 @@ PROGRAM main
  ! Fname='aaaaaaaaaaaaaaaaaaaaaaaaa',e,'.dat'
 ! fname='bcdddddd'  !only print b 
   
-  
-OPEN(300,FILE=fname)     
-write(300,*)'good'
-Close(300)
+OPEN(10, file='tianli\output2.dat', status='new')
+!status='new'=non exist ,'old'=exist
+CLOSE(10)
+
+!OPEN(300,FILE=fname)
+!write(300,*),'hello'
+!write(300,200)array
+!
+!Close(300)
+!!!!200 format(3i5.2)!1i5,2 means one column, 2i5,2 means 2 colume
   DEALLOCATE (array)
 print*,'hello'
 END PROGRAM main  
